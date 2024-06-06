48346c0209d1ce33635551a21b76b995d984c380
Jose Vitor <josevitorrodrigues17@gmail.com>
2024-06-06 11:33:10 -0300

    exercices

--- basic/mercadin.go
+++ basic/mercadin.go
@@ -5,27 +5,28 @@ import (
 	"strconv"
 )
 
-func main() {
-	cardapio := [5]float32{7.00, 10.00, 8.50, 6.00, 6.00}
-	var resposta int
+// func main() {
+// 	cardapio := [5]float32{7.00, 10.00, 8.50, 6.00, 6.00}
+// 	var resposta int
+	
 
-	f.Println("Cardápio:")
-	f.Printf(" 0-Cachorro-Quente: %.2f\n 1-Pizza: %.2f\n 2-Hamburguer: %.2f\n 3-Guaraná %.2f\n 4-Coca-Cola: %.2f\n", cardapio[0], cardapio[1], cardapio[2], cardapio[3], cardapio[4])
-	f.Println("Digite o código dos lanches desejados:")
-	f.Scanf("%d", &resposta)
+// 	f.Println("Cardápio:")
+// 	f.Printf(" 0-Cachorro-Quente: %.2f\n 1-Pizza: %.2f\n 2-Hamburguer: %.2f\n 3-Guaraná %.2f\n 4-Coca-Cola: %.2f\n", cardapio[0], cardapio[1], cardapio[2], cardapio[3], cardapio[4])
+// 	f.Println("Digite o código dos lanches desejados:")
+// 	f.Scanf("%d", &resposta)
 
-	respostaArray := intToArray(resposta)
+// 	respostaArray := intToArray(resposta)
 
-	var total float32
+// 	var total float32
 
-	for _, digit := range respostaArray {
-		if digit >= 0 && digit < len(cardapio) {
-			total += cardapio[digit]
-		}
-	}
+// 	for _, digit := range respostaArray {
+// 		if digit >= 0 && digit < len(cardapio) {
+// 			total += cardapio[digit]
+// 		}
+// 	}
 
-	f.Printf("Total a pagar: %.2f", total)
-}
+// 	f.Printf("Total a pagar: %.2f", total)
+// }
 
 func intToArray(num int) []int {
 	numString := strconv.Itoa(num)
